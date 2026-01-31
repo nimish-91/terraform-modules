2f578dc0a496845d8c10fa5cf9b132e539669ad4
unknown <you@example.com>
2026-01-31 11:28:36 +0530

    inbound port is added

--- security-group/main.tf
+++ security-group/main.tf
@@ -13,7 +13,14 @@ resource "aws_security_group" "this" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
